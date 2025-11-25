802d8eae04fa88f7061e0eae1a57a7053b3de3d2
alireza1045 <fifa24alireza24@gmail.com>
2025-11-25 16:23:26 +0330

    upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for NAROON Website
 // Cache strategy: Cache First with Network Fallback
 
-const CACHE_VERSION = 'v8';
+const CACHE_VERSION = 'v9';
 const CACHE_NAME = `naroonsignmaker-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `naroonsignmaker-runtime-${CACHE_VERSION}`;
 
